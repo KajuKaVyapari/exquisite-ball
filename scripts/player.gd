9ba7c4fcf9516ec82769a0b87da1d1383713bad3
KajuKaVyapari <rehan.daphedar@gmail.com>
2020-08-01 21:32:19 +0530

    *WIP* setting up time switching + set up layer names

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,14 @@ var gravity = 15
 var gravity_direction = "down"
 
 
+func _ready():
+	
+	get_parent().get_node("past_tilemap").visible = false
+
+
 func _physics_process(delta):
 	
+	# Gravity Switching
 	if Input.is_action_just_pressed("ui_down"):
 		gravity_direction = "down"
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
@@ -18,3 +24,6 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("ui_right"):
 		gravity_direction = "right"
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.RIGHT)
+	
+	
+	if Input.is
