c4dc4a75387aa4efcfaf7e023e16aa40e8a9ae1d
KajuKaVyapari <rehan.daphedar@gmail.com>
2020-08-01 16:45:42 +0530

    added basic gravity switching

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+
+var gravity = 15
+var gravity_direction = "down"
+
+
+func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("ui_down"):
+		gravity_direction = "down"
+		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
+	elif Input.is_action_just_pressed("ui_up"):
+		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
+	elif Input.is_action_just_pressed("ui_left"):
+		gravity_direction = "left"
+		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.LEFT)
+	elif Input.is_action_just_pressed("ui_right"):
+		gravity_direction = "right"
+		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.RIGHT)
