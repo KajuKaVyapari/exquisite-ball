68e2a57e0a8dfe7fa6c37b3223abfc90995c5ccc
KajuKaVyapari <rehan.daphedar@gmail.com>
2020-08-02 18:50:09 +0530

    +completely fixed ball stuck on wall bug +added base level

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,13 +20,18 @@ func _physics_process(delta):
 	# Gravity Switching
 	if Input.is_action_just_pressed("ui_down"):
 		gravity_direction = "down"
+		apply_central_impulse(Vector2.DOWN * 50)
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
 	elif Input.is_action_just_pressed("ui_up"):
+		gravity_direction = "up"
+		apply_central_impulse(Vector2.UP * 50)
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
 	elif Input.is_action_just_pressed("ui_left"):
 		gravity_direction = "left"
+		apply_central_impulse(Vector2.LEFT * 50)
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.LEFT)
 	elif Input.is_action_just_pressed("ui_right"):
+		apply_central_impulse(Vector2.RIGHT * 50)
 		gravity_direction = "right"
 		Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.RIGHT)
 	
@@ -37,7 +42,6 @@ func _physics_process(delta):
 		# Rewind Mechanic
 		
 		Engine.time_scale = 0.8
-		add_central_force(Vector2(0.001, 0.001)) # Fixes ball stuck bug
 		
 		if time == "present":
 			collision_layer = 4
@@ -56,5 +60,5 @@ func _physics_process(delta):
 			time = "present"
 
 
-func _on_tilemap_tween_completed(object, key):
+func _on_tilemap_tween_tween_completed():
 	Engine.time_scale = 1
